5fee4dc83a023bb8d2e3ae57e49ad907f6cb464d
elvischikudzah <elvischikudza@gmail.com>
2023-10-07 06:21:09 -0700

    s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,4 +4,5 @@ int main(void)
 {
 char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, 59);  /* 59 is the length of the message */
-return (1); 
+return (1);
+}
